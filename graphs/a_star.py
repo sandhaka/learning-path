e5104c4b295a6410bef0c6c0035e57cbfac7d8bd
Matteo Tosato <matteo.tosato7@gmail.com>
2023-12-13 17:45:00 +0100

    maze2graph

--- graphs/a_star.py
+++ graphs/a_star.py
@@ -1,31 +1,52 @@
 import argparse
-from utils import maze
+import utils.maze as maze_generator
+import matplotlib.pyplot as plt
+
+from heapq import heappush, heappop
 from colorama import init, Fore
 
-parser = argparse.ArgumentParser("a-star")
-parser.add_argument("--maze", help="The size of the map square (side length)", type=int, default=8)
+parser = argparse.ArgumentParser("a-star", description="A* search algorithm collection")
+parser.add_argument("--maze", help="The size of the map square (side length)", type=int, default=35)
 
 args = parser.parse_args()
 
-wall = 1
-passage = 0
+mz = maze_generator.MazeBlocks
 size = args.maze
 
 
 def print_maze_on_console(m: list[list[int]]) -> None:
     for i in range(0, len(m)):
         for j in range(0, len(m[0])):
-            if m[i][j] == "u":
-                print(Fore.WHITE, f"{m[i][j]}", end="    ")
-            elif m[i][j] == passage:
+            if m[i][j] == mz.PASSAGE:
                 print(Fore.GREEN, f"{m[i][j]}", end="    ")
             else:
                 print(Fore.RED, f"{m[i][j]}", end="    ")
         print("\n")
 
 
+def a_star_search(g: dict) -> list:
+    pass
+
+
+# def plot_graph_verification(m, g) -> None:
+#     plt.figure(figsize=(8, 8))
+#     plt.ion()  # Turn on interactive mode
+#     for node in g.keys():
+#         plt.clf()  # Clear the previous plot
+#         plt.imshow(m, cmap="binary", interpolation="none", origin="upper")
+#         passage = [node] + g[node]
+#         for point in passage:
+#             plt.scatter(point[1], point[0], marker="o", color="red")
+#         plt.title(f"Node {node}: {g[node]}")
+#         plt.pause(5)
+#     plt.ioff()  # Turn off interactive mode
+#     plt.show()
+
+
 # Init colorama
 init()
 # Get a maze sample
-maze_board = maze.gen(size)
-print_maze_on_console(maze_board)
+maze = maze_generator.gen(size)
+adjacency_list = maze_generator.maze2graph(maze)
+
+print_maze_on_console(maze)
--- utils/maze.py
+++ utils/maze.py
@@ -1,7 +1,10 @@
 import random
 
-wall = 1
-passage = 0
+
+class MazeBlocks:
+    WALL = 1
+    PASSAGE = 0
+
 
 """
 Generate a random maze to use as graph map for shortest path algorithm searches
@@ -10,38 +13,62 @@ https://en.wikipedia.org/wiki/Maze_generation_algorithm#Iterative_implementation
 """
 
 
-def gen(size: int):
+def gen(size: int) -> list[list[int]]:
     def get_neighbors(r, c):
         _n = []
         for dx, dy in [(-2, 0), (2, 0), (0, -2), (0, 2)]:
             x, y = r + dx, c + dy
-            if 0 <= x < size and 0 <= y < size and maze_map[x][y] != passage:
+            if 0 <= x < size and 0 <= y < size and maze_map[x][y] != MazeBlocks.PASSAGE:
                 _n.append((x, y))
         return _n
 
     size = size * 2 + 1
-    maze_map = [[wall] * size for _ in range(size)]
+    maze_map = [[MazeBlocks.WALL] * size for _ in range(size)]
 
     stack = [(1, 1)]
-    maze_map[1][1] = passage
+    maze_map[1][1] = MazeBlocks.PASSAGE
 
     while stack:
         i, j = stack[-1]
-        maze_map[i][j] = passage
+        maze_map[i][j] = MazeBlocks.PASSAGE
 
         neighbors = get_neighbors(i, j)
 
         if neighbors:
             neighbor = random.choice(neighbors)
 
-            maze_map[(i + neighbor[0]) // 2][(j + neighbor[1]) // 2] = passage
+            maze_map[(i + neighbor[0]) // 2][(j + neighbor[1]) // 2] = MazeBlocks.PASSAGE
 
             stack.append(neighbor)
         else:
             stack.pop()
 
     # Create the entrance and the exit
-    maze_map[0][1] = passage
-    maze_map[size - 1][size - 2] = passage
+    maze_map[0][1] = MazeBlocks.PASSAGE
+    maze_map[size - 1][size - 2] = MazeBlocks.PASSAGE
 
     return maze_map
+
+
+"""
+Transform maze to graph
+"""
+
+
+def maze2graph(maze: list[list[int]]) -> dict:
+    graph = {}
+
+    def get_neighbors(i, j, size):
+        for dx, dy in [(-2, 0), (2, 0), (0, -2), (0, 2)]:
+            if 0 <= i + dx < size - 1 and 0 <= j + dy < size - 1:
+                yield i + dx, j + dy
+
+    for r in range(1, len(maze) - 1, 2):
+        for c in range(1, len(maze[0]) - 1, 2):
+            if (r, c) not in graph:
+                graph[(r, c)] = []
+            for neighbor in get_neighbors(r, c, len(maze)):
+                if maze[(r + neighbor[0]) // 2][(c + neighbor[1]) // 2] == MazeBlocks.PASSAGE:
+                    graph[(r, c)].append(neighbor)
+
+    return graph
